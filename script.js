19ef31f58650f37826aa90ddb415ecee4f433da9
Samuel <muel.h.o33@gmail.com>
2024-03-11 10:28:29 -0300

    Alterando javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function carregar() {
     var msg = window.document.querySelector('div#msg') //os dois fazem a mesma função
-    var img = window.document.getElementById('imagem') // mesma função que o outro de cima
+    var img = window.document.getElementById('foto') // mesma função que o outro de cima
     var data = new Date()
     var hora = data.getHours()
     var minutos = data.getMinutes()
